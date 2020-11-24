e825aa45a7a8354c10874407282a04bd08328962
Tom Hewitt <tommhewitt@gmail.com>
2020-11-24 19:28:09 +0000

    chore: project init

---
+++ src/main/java/iamtomhewitt/clob/ChristmasListOrganiserBackendApplication.java
@@ -0,0 +1,11 @@
+package iamtomhewitt.clob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristmasListOrganiserBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ChristmasListOrganiserBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/iamtomhewitt/clob/christmaslistorganiserbackend/ChristmasListOrganiserBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package iamtomhewitt.clob.christmaslistorganiserbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChristmasListOrganiserBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
