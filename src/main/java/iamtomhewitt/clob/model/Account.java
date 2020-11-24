9baaa7005a4886928bce25bfa76a20f8d518d66f
Tom Hewitt <tom.hewitt@williamhill.com>
2020-11-24 21:42:02 +0000

    chore(#1): implement getting and saving an account

--- src/main/java/iamtomhewitt/clob/model/Account.java
+++ src/main/java/iamtomhewitt/clob/model/Account.java
@@ -1,5 +1,6 @@
 package iamtomhewitt.clob.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,9 +11,9 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Builder
 @AllArgsConstructor
 public class Account {
-    private String username;
-    private String password;
     private String email;
+    @JsonIgnore
+    private String password;
     private String firstName;
     private String lastName;
 
